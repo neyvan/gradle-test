40169e99dd608aa4079ed5e7e8afe5114db683f0
neyvan <essiorho@gmail.com>
2020-11-14 23:02:28 +0300

    change to hello

--- src/main/java/ru/neyvan/gradlet/App.java
+++ src/main/java/ru/neyvan/gradlet/App.java
@@ -5,10 +5,11 @@ package ru.neyvan.gradlet;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println("Hello again!");
     }
 }
